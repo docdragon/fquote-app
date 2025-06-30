a33ed71065445cd1b1bd76d3d5f5b856d9d3f6f2
docdragon <nguyenminhthien.pr98@gmail.com>
2025-06-30 12:11:30 +0700

    Add files via upload

---
+++ api/_utils-for-api.js
@@ -0,0 +1,44 @@
+/**
+ * @file _utils-for-api.js
+ * @description Tiện ích cho các API endpoint chạy trên môi trường Node.js.
+ * KHÔNG SỬ DỤNG TRỰC TIẾP TRÊN CLIENT.
+ */
+
+const formatCurrency = (number, showSymbol = true) => {
+    if (typeof number !== 'number' || isNaN(number)) return showSymbol ? '0 ₫' : '0';
+    const formatted = new Intl.NumberFormat('vi-VN').format(Math.round(number));
+    return showSymbol ? `${formatted} ₫` : formatted;
+};
+
+const formatDate = (dateInput) => {
+    if (!dateInput) return '';
+    try {
+        const date = new Date(dateInput);
+        if (isNaN(date.getTime())) return '';
+        return new Intl.DateTimeFormat('vi-VN', {
+            day: '2-digit',
+            month: '2-digit',
+            year: 'numeric'
+        }).format(date);
+    } catch (e) {
+        return '';
+    }
+};
+
+const numberToRoman = (num) => {
+    if (typeof num !== 'number' || isNaN(num) || num <= 0) return '';
+    const roman = { M: 1000, CM: 900, D: 500, CD: 400, C: 100, XC: 90, L: 50, XL: 40, X: 10, IX: 9, V: 5, IV: 4, I: 1 };
+    let str = '';
+    for (let i of Object.keys(roman)) {
+        let q = Math.floor(num / roman[i]);
+        num -= q * roman[i];
+        str += i.repeat(q);
+    }
+    return str;
+};
+
+module.exports = {
+    formatCurrency,
+    formatDate,
+    numberToRoman,
+};
